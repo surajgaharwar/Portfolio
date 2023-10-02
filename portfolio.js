68b38c31c38a308dfd31ba210730ef456f8ca52b
surajgaharwar <133503447+surajgaharwar@users.noreply.github.com>
2023-10-02 23:30:52 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,38 @@
+// Constants
+const heroSection = document.querySelector(".section-hero");
+const contactUs = document.querySelector(".navbar-link contact-link");
+const biodata = document.querySelector(".section-bio-data");
+
+// Scroll To Top
+// const scrollElement = document.querySelector(".scrollToTop");
+
+// const scrollTop = () => {
+//     heroSection.scrollIntoView({behavior: "smooth"});
+// }
+
+// scrollElement.addEventListener("click", scrollTop);
+
+
+// ContactUs 
+// const scrollTop = () => {
+// contactUS.scrollIntoView({behavior: "smooth"});
+// }
+
+// contactUs.addEventListener("click", scrollTop);
+
+
+// const observer = new IntersectionObserver(
+//     (entries) => {
+//         const ent = entries[0];
+//         console.log(ent);
+//         !ent.isIntersecting
+//         ? document.body.classList.add("sticky")
+//         : document.body.classList.remove("sticky");
+//     },
+//     {
+//         root: null,
+//         threshold: 0,
+//     }
+// );
+
+// observer.observe(heroSection);
